c3a1f5005f3b77ece9272ab637cb5ddc394c54c1
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2015-03-31 20:00:45 +0100

    delete everything & start again

--- src/berta.cpp
+++ src/berta.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-
-#include "../geometry/Point.h"
-using namespace std;
 
 int main() {
-	cout << Point::sayHello() << endl; // prints Hello, world!
+	// create a scene:
+	// 1. scene itself
+	// 2. world objects
+	// 3. camera & camera position
+
+	// create a renderer
+	// 1. ask the renderer to render the scene
 	return 0;
 }
