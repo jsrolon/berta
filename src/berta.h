52c6c459c7b2106a307822f0e2059ead23bfa9cd
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2015-04-02 17:45:36 +0100

    add geometry from pbrt, perfect compilation

--- src/berta.h
+++ src/berta.h
@@ -1,13 +1,169 @@
 /*
- * berta.h
- *
- *  Created on: Mar 31, 2015
- *      Author: jsrolon
+    pbrt source code Copyright(c) 1998-2012 Matt Pharr and Greg Humphreys.
+
+    This file is part of pbrt.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are
+    met:
+
+    - Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    - Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+    IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+    TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+    HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+    LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
  */
 
 #ifndef BERTA_H_
 #define BERTA_H_
 
+// Global Include Files
+#include <math.h>
+#include <stdlib.h>
+#define _GNU_SOURCE 1
+#include <stdio.h>
+#include <string.h>
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <assert.h>
+#include <algorithm>
+using std::min;
+using std::max;
+using std::swap;
+using std::sort;
 
+/**
+ * Forward declarations for all classes
+ */
+// Camera
+class Camera;
+class ViewPlane;
+
+// Geometry
+class Point;
+class Vector;
+class Ray;
+class Normal;
+class BBox;
+
+// Utils
+class Intersection;
+class Sampler;
+class Transform;
+
+// Global Inline Functions
+inline float Lerp(float t, float v1, float v2) {
+    return (1.f - t) * v1 + t * v2;
+}
+
+
+inline float Clamp(float val, float low, float high) {
+    if (val < low) return low;
+    else if (val > high) return high;
+    else return val;
+}
+
+
+inline int Clamp(int val, int low, int high) {
+    if (val < low) return low;
+    else if (val > high) return high;
+    else return val;
+}
+
+
+inline int Mod(int a, int b) {
+    int n = int(a/b);
+    a -= n*b;
+    if (a < 0) a += b;
+    return a;
+}
+
+
+inline float Radians(float deg) {
+    return ((float)M_PI/180.f) * deg;
+}
+
+
+inline float Degrees(float rad) {
+    return (180.f/(float)M_PI) * rad;
+}
+
+
+inline float Log2(float x) {
+    static float invLog2 = 1.f / logf(2.f);
+    return logf(x) * invLog2;
+}
+
+
+inline int Floor2Int(float val);
+inline int Log2Int(float v) {
+    return Floor2Int(Log2(v));
+}
+
+
+inline bool IsPowerOf2(int v) {
+    return v && !(v & (v - 1));
+}
+
+
+inline uint32_t RoundUpPow2(uint32_t v) {
+    v--;
+    v |= v >> 1;    v |= v >> 2;
+    v |= v >> 4;    v |= v >> 8;
+    v |= v >> 16;
+    return v+1;
+}
+
+
+inline int Floor2Int(float val) {
+    return (int)floorf(val);
+}
+
+
+inline int Round2Int(float val) {
+    return Floor2Int(val + 0.5f);
+}
+
+
+inline int Float2Int(float val) {
+    return (int)val;
+}
+
+
+inline int Ceil2Int(float val) {
+    return (int)ceilf(val);
+}
+
+inline bool Quadratic(float A, float B, float C, float *t0, float *t1) {
+    // Find quadratic discriminant
+    float discrim = B * B - 4.f * A * C;
+    if (discrim < 0.) return false;
+    float rootDiscrim = sqrtf(discrim);
+
+    // Compute quadratic _t_ values
+    float q;
+    if (B < 0) q = -.5f * (B - rootDiscrim);
+    else       q = -.5f * (B + rootDiscrim);
+    *t0 = q / A;
+    *t1 = C / q;
+    if (*t0 > *t1) swap(*t0, *t1);
+    return true;
+}
 
 #endif /* BERTA_H_ */
---
+++ src/utils/geometry.cpp
@@ -0,0 +1,9 @@
+/*
+ * geometry.cpp
+ *
+ *  Created on: Apr 2, 2015
+ *      Author: jsrolon
+ */
+
+#include "geometry.h"
+
